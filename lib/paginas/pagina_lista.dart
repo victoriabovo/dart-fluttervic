2d192e917b13c106172d23e9fa5a3f66a67cc427
VICTORIA REGINA BOVO BATISTON <victoria.9946@aluno.pr.senac.br>
2023-07-04 10:08:54 -0300

    ola

--- lib/paginas/pagina_lista.dart
+++ lib/paginas/pagina_lista.dart
@@ -1,84 +1,89 @@
 import 'package:flutter/material.dart';
 
-// final TextEditingController email_controle = TextEditingController();
+final TextEditingController email_controle = TextEditingController();
 
 class Pagina_Lista extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
-      //Scaffold è um Widget que fornece uma estrutura visual basica para um aplicativo
       body: Center(
-        child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Row(
               children: [
-                Row(
-                  children: [
-                    Expanded(
-                      child: TextField(
-                        decoration: InputDecoration(
-                            border: OutlineInputBorder(),
-                            labelText: "Adicione uma tarefa",
-                            hintText: "digite aqui"),
-                      ),
-                    ),
-                    SizedBox(
-                      width: 8,
-                    ),
-                    ElevatedButton(
-                      onPressed: () {},
-                      style: ElevatedButton.styleFrom(
-                          primary: Color.fromARGB(255, 114, 8, 128),
-                          padding: EdgeInsets.all(20)),
-                      child: Icon(
-                        Icons.add,
-                        size: 30,
-                      ),
-                    ),
-                  ],
+                Expanded(
+                  child: TextField(
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: "Adicione uma tarefa",
+                        hintText: "Digite aqui"),
+                  ),
                 ),
-                SizedBox(
-                  height: 10,
-                ),
-                Row(
-                  children: [
-                    Expanded(
-                      child: Text(" Você tem 0 tarefas pendentes"),
-                    ),
-                    SizedBox(
-                      width: 8, /* Espaçamento entre o texto e o botão */
-                    ),
-                    ElevatedButton(
-                      onPressed: () {},
-                      style: ElevatedButton.styleFrom(
-                          primary: Color.fromARGB(255, 114, 8, 128),
-                          padding: EdgeInsets.all(20)),
-                      child: Text("Limpar"),
-                    ),
-                  ],
-                ),
-                SizedBox(
-                  height: 40,
-                ),
-                ListView(),
-                SizedBox(
-                  height: 40,
+                SizedBox(width: 7),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    primary: Color.fromARGB(255, 0, 192, 250),
+                  ),
+                  child: Icon(
+                    Icons.add,
+                    size: 30,
+                  ),
                 ),
               ],
-            )),
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            Row(
+              children: [
+                Expanded(
+                    child: TextField(
+                  decoration: InputDecoration(
+                    labelText: "Você possui 0 tarefas pendentes",
+                  ),
+                )),
+                SizedBox(width: 7),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    primary: Color.fromARGB(255, 0, 192, 250),
+                  ),
+                  child: Text("Limpar"),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            SizedBox(
+              height: 120,
+              child: ListView(
+                children: [
+                  Container(
+                    color: Color.fromARGB(255, 0, 171, 250),
+                    width: 50,
+                    height: 50,
+                  ),
+                  Container(
+                    color: Color.fromARGB(255, 250, 0, 104),
+                    width: 50,
+                    height: 50,
+                  ),
+                  Container(
+                    color: Color.fromARGB(255, 250, 0, 250),
+                    width: 50,
+                    height: 50,
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 40,
+            ),
+          ],
+        ),
       ),
     );
   }
 }
-
-/*
-void entrar() {
-  String texto = email_controle
-      .text; //obtém o texto inserido no campo de entrada associado ao "controlar".
-  print(texto);
-  email_controle
-      .clear(); //limpa o comapo de entrada,removendo qualquer texto atualmente presente nele.
-}
-
-*/
-
